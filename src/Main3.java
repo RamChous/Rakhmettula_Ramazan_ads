f9c1e60b6b1df6635b9ac4b76a1dd49f1cc58a47
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:32:08 +0500

    initial commit

--- src/Main3.java
+++ src/Main3.java
@@ -4,20 +4,20 @@ public class Main3 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        if (problem3(n, n / 2)) {
+        if (PrimeOrComp(n, n / 2)) {
             System.out.println("Prime");
         } else {
             System.out.println("Composite");
         }
         scn.close();
     }
-    public static boolean problem3(int n, int d) {
+    public static boolean PrimeOrComp(int n, int d) {
         if (n <= 1)
             return false;
         if (d == 1)
             return true;
         if (n % d == 0)
             return false;
-        return problem3(n, d - 1);
+        return PrimeOrComp(n, d - 1);
     }
 }
