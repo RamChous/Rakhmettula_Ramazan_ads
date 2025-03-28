fd56d85ff3d93b6cc5af8db38f0bff906c709086
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:25:55 +0500

    initial commit

--- src/Main7.java
+++ src/Main7.java
@@ -1,27 +1,20 @@
 import java.util.Scanner;
 
 public class Main7 {
-    public Main7() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
         problem7(str, "");
         scn.close();
     }
-
     public static void problem7(String str, String result) {
         if (str.isEmpty()) {
             System.out.println(result);
         }
-
         for(int i = 0; i < str.length(); ++i) {
             char ch = str.charAt(i);
-            String var10000 = str.substring(0, i);
-            String remain = var10000 + str.substring(i + 1);
+            String remain = str.substring(0, i) + str.substring(i + 1);
             problem7(remain, result + ch);
         }
-
     }
 }
