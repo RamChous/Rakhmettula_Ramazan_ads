e2ae4ab4385e0579075c13005ec68e36d371b971
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:28:24 +0500

    initial commit

--- src/Main8.java
+++ src/Main8.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
 public class Main8 {
-    public Main8() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         String s = scn.nextLine();
-        if (problem8(s, 0)) {
+        if (AllDigits(s, 0)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
-
         scn.close();
     }
-
-    public static boolean problem8(String s, int i) {
-        if (s.isEmpty()) {
+    public static boolean AllDigits(String s, int i) {
+        if (s.isEmpty())
             return false;
-        } else if (s.length() == i) {
+        if (s.length() == i)
             return true;
-        } else {
-            return !Character.isDigit(s.charAt(i)) ? false : problem8(s, i + 1);
-        }
+        if (!Character.isDigit(s.charAt(i)))
+            return false;
+        return AllDigits(s, i + 1);
     }
 }
