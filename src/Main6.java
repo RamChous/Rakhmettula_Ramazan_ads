fa327ca6fdd0c4acf59183851fa5e5071b5375df
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:24:39 +0500

    initial commit

--- src/Main6.java
+++ src/Main6.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class Main6 {
-    public Main6() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
@@ -11,8 +8,9 @@ public class Main6 {
         System.out.println(problem6(a, n));
         scn.close();
     }
-
     public static int problem6(int a, int n) {
-        return n == 1 ? a : problem6(a, n - 1) * a;
+        if (n == 1)
+            return a;
+        return problem6(a, n - 1) * a;
     }
 }
