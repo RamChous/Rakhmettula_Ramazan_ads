a0d005eec86927ce9052e32a0d7a3cd5a32bd3a4
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:33:05 +0500

    initial commit

--- src/Main1.java
+++ src/Main1.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 
 public class Main1 {
-    public Main1() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] nums = new int[n];
-
-        for(int i = 0; i < n; ++i) {
+        for(int i = 0; i < n; ++i)
             nums[i] = scn.nextInt();
-        }
-
-        System.out.println(problem1(n, nums));
+        System.out.println(minimum(n, nums));
         scn.close();
     }
-
-    public static int problem1(int n, int[] nums) {
+    public static int minimum(int n, int[] nums) {
         if (n == 1)
             return nums[0];
-        int min = problem1(n - 1, nums);
+        int min = minimum(n - 1, nums);
         if (nums[n - 1] < min) {
             return nums[n - 1];
         } else {
