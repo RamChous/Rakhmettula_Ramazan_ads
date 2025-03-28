98c4ebfd6633845e1788e6d30679d2434947d3ef
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:31:02 +0500

    initial commit

--- src/Main5.java
+++ src/Main5.java
@@ -4,12 +4,12 @@ public class Main5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        System.out.println(problem5(n));
+        System.out.println(fibonacci(n));
         scn.close();
     }
-    public static int problem5(int n) {
+    public static int fibonacci(int n) {
         if (n == 1 || n == 2)
             return 1;
-        return problem5(n - 1) + problem5(n - 2);
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
