03d581921c743b3f84593157222102deb589e16d
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:31:39 +0500

    initial commit

--- src/Main4.java
+++ src/Main4.java
@@ -4,12 +4,12 @@ public class Main4 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        System.out.println(problem4(n));
+        System.out.println(factorial(n));
         scn.close();
     }
-    public static int problem4(int n) {
+    public static int factorial(int n) {
         if (n == 0 || n == 1)
             return 1;
-        return problem4(n - 1) * n;
+        return factorial(n - 1) * n;
     }
 }
