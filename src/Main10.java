c374597b289bc0143bec8ef6afd30de1b283ab34
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:34:26 +0500

    initial commit

--- src/Main10.java
+++ src/Main10.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
 public class Main10 {
-    public Main10() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
         int b = scn.nextInt();
-        System.out.println(problem10(a, b));
+        System.out.println(gcd(a, b));
         scn.close();
     }
-
-    public static int problem10(int a, int b) {
-        return b == 0 ? a : problem10(b, a % b);
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 }
