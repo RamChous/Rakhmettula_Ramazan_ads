aad92e97a346a7e59e0ea6c1beeed49af6014e10
roma2 <rahmettularamazan@gmail.com>
2025-03-28 18:23:04 +0500

    initial commit

--- src/Main4.java
+++ src/Main4.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Main4 {
-    public Main4() {
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         System.out.println(problem4(n));
         scn.close();
     }
-
     public static int problem4(int n) {
-        return n != 0 && n != 1 ? problem4(n - 1) * n : 1;
+        if (n == 0 || n == 1)
+            return 1;
+        return problem4(n - 1) * n;
     }
 }
